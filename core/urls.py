e68d6d4d2dbe4d6f889263e656ff2ba82d1e013b
jiromba <nomamdeprogramador@gmail.com>
2020-08-07 20:45:01 -0300

    First comment

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,12 @@ urlpatterns=[
   #  path('',views.index,name='index'),
     path('pet/list/',views.list_all_pet,name='listagem pets'),
     path('pet/user/',views.list_user_pet,name='MEUS PETS'),
+    path('pet/detail/<id>/', views.pet_detail),
+    path('pet/register/', views.register_pet),
+    path('pet/register/submit',views.set_pet),
+    path('pet/delete/<slug:id>/',views.delete_pet),
     path('login/',views.login_user,name='LOGIN'),
-    path('login/submit',views.submit_login),
+    path('login/submit/',views.submit_login),
     path('logout/',views.logout_user),
     path('',RedirectView.as_view(url='pet/list/')),
 ]
--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Pet
+from .models import *
 # Create your views here.
 @csrf_protect
 @login_required(login_url='/login/')
@@ -15,8 +15,6 @@ def submit_login(request):
     if request.POST:
         username=request.POST.get('username')
         passoword=request.POST.get('password')
-        print(username)
-        print(passoword)
         user=authenticate(username='username',passoword='password')
         if user is not None:
             login(request,user)
@@ -35,17 +33,60 @@ def submit_login(request):
         else:
             messages.error(request, 'User/Passowrd WRONG, Please Try again')
     return redirect('/login/')
+
+def pet_detail(request, id):
+    pet = Pet.objects.get(active=True, id=id)
+    return render(request, 'index/pet.html', {'pet':pet})
+
 def logout_user(request):
     logout(request)
     return redirect('/login/')
 def list_all_pet(request):
     pet=Pet.objects.filter(active=True)
-    print(pet.query)
     context={'pet':pet}
     return render(request,'index/list.html',context )
+
+
 def list_user_pet(request):
     pet=Pet.objects.filter(active=True,user=request.user)
     return render(request,'index/list.html',{'pet':pet})
 
-def pet_detail(request,id):
-    pass 
+def register_pet(request):
+    return render(request,'index/register.html')
+
+
+
+@login_required(login_url='/login/')
+def delete_pet(request, id):
+    pet = Pet.objects.get(id=id)
+    print(pet.id)
+    if pet.user == request.user:
+        pet.delete()
+    return redirect('/')
+
+
+
+@login_required(login_url='/login/')
+def set_pet(request):
+    city = request.POST.get('city')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    description = request.POST.get('description')
+    file = request.FILES.get('file')
+    user = request.user
+    pet_id = request.POST.get('pet_id')
+    if pet_id:
+        pet = Pet.objects.get(id=pet_id)
+        if user == pet.user:
+            pet.email = email
+            pet.phone = phone
+            pet.city = city
+            pet.description = description 
+            if file:
+                pet.photo = file
+            pet.save()
+    else:
+        pet = Pet.objects.create(email=email, phone=phone, city=city, description=description,
+                                user=user, photo=file)
+    url = '/pet/detail/{}/'.format(pet.id)
+    return redirect(url)
